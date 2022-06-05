73a795038283054be6a004ed0ab8b811cb59e3d0
zedsoul <1484908116@qq.com>
2022-06-05 13:11:48 +0800

    test

---
+++ app/Http/Controllers/testController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class testController extends Controller
+{
+    //
+    public function test(){
+        echo "1111";
+        return  "222";
+    }
+}
